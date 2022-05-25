83ae04f8b7c6be443f156159d5243fc7e2cda330
Wildannbotz <96801218+Wildanbotz@users.noreply.github.com>
2022-05-25 22:51:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283897021821','628113048247','6288292024190']
-global.premium = ['6283897021821']
-global.packname = 'Nandi Bot-MD'
+global.owner = ['6285880486685','6285880486685','6285880486685']
+global.premium = ['6285880486685']
+global.packname = 'WildanBot-MD'
 global.author = 'Whatsap Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
